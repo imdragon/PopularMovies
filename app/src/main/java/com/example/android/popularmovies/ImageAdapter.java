ffc64a0ba388d438b280aa9bd2257f56e8ab5e04
imdragon <i.m.dragon@gmail.com>
2016-03-04 15:44:34 -0800

    Added request to movieDB Logged to see if I got response Response working

--- app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
@@ -2,12 +2,11 @@ package com.example.android.popularmovies;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -21,11 +20,13 @@ public class ImageAdapter extends BaseAdapter {
 
     public ImageAdapter(Context c) {
         mContext = c;
+        new RequestPopularMovies().execute(null, null, null);
     }
 
     @Override
     public int getCount() {
-        return moviesPosters.length();
+//        return moviesPosters.length();
+        return 0;
     }
 
     @Override
@@ -42,12 +43,33 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
     }
-    private class RequestPopularMovies extends AsyncTask<Void, Void, Void>{
+
+    private class RequestPopularMovies extends AsyncTask<Void, Void, Void> {
+        StringBuilder total = new StringBuilder();
+        String apiKey = mContext.getResources().getString(R.string.apiKey);
 
         @Override
         protected Void doInBackground(Void... params) {
+// using the apikey in a seperate string file to protect the apikey
+            try {
+                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + mContext.getResources().getString(R.string.apiKey));
+                // making url request and sending it to be read
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // preparing a reader to go through the response
+                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                // below allows for controlled reading of potentially large text
+
+                String line;
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                }
+                // checking if getting back something
+                Log.e("Response", total.toString());
 
 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
