9e43cc00886f4f1b871e8716c0222af9a75df634
imdragon <i.m.dragon@gmail.com>
2016-03-04 15:26:16 -0800

    API key is in resource not shared Started an AsyncTask to pull from API

--- app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
@@ -1,24 +1,31 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
-/**
- * Created by Admin on 3/4/2016.
- */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ImageAdapter extends BaseAdapter {
     public Context mContext;
+    private String moviesPosters;
+
 
     public ImageAdapter(Context c) {
         mContext = c;
     }
 
-
     @Override
     public int getCount() {
-        return 0;
+        return moviesPosters.length();
     }
 
     @Override
@@ -35,4 +42,41 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
     }
+    private class RequestPopularMovies extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+            return null;
+        }
+    }
+
+
+//    private class RequestPopularMovies extends AsyncTask<Void, Void, Void> {
+//        StringBuilder total = new StringBuilder();
+//
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            try {
+//                // using the apikey in a seperate string file to protect the apikey
+//                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + getResources().getString(R.string.apiKey));
+//                // making url request and sending it to be read
+//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                // preparing a reader to go through the response
+//                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//                // below allows for controlled reading of potentially large text
+//                String line;
+//                while ((line = r.readLine()) != null) {
+//                    total.append(line);
+//                }
+//                // experimental stuff below
+//
+//                JSONObject popArray = new JSONObject(total.toString());
+//                JSONArray movies = popArray.getJSONArray(MOVIE_BLOCKS);
+//            }
+//            return null;
+//        }
+//
+//    }
 }
