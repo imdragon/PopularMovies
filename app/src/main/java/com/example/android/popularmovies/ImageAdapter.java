8f8165a54e8bce8ed2324467fb13769b9dcb0851
imdragon <i.m.dragon@gmail.com>
2016-03-04 19:38:55 -0800

    Branched out to move Async to main activity and pass it to the imageadapter instead

--- app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,28 +10,15 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class ImageAdapter extends BaseAdapter {
     public Context mContext;
-    private ArrayList<String> moviePosterAddress = new ArrayList<>();
-    private JSONObject popArray;
-    private JSONArray movies;
-    final String MOVIE_POSTER = "poster_path";
-    private String[] moviePathString = new String[30];
-    Boolean flag = false;
+    private String[] moviePathString;
 
-    public ImageAdapter(Context c) {
-        new RequestPopularMovies().execute(null, null, null);
+    public ImageAdapter(Context c, ArrayList<String> moviePosterAddress) {
         mContext = c;
+        moviePathString = moviePosterAddress.toArray(new String[moviePosterAddress.size()]);
     }
 
     @Override
@@ -75,58 +61,4 @@ public class ImageAdapter extends BaseAdapter {
         return imageView;
     }
 
-// added ImageView take it out if it doesn't work! 3/4
-    private class RequestPopularMovies extends AsyncTask<ImageView, Void, Void> {
-        StringBuilder total = new StringBuilder();
-        final String MOVIE_BLOCKS = "results";
-        final String MOVIE_TITLE = "original_title";
-        //        final String MOVIE_POSTER = "poster_path";
-        final String MOVIE_OVERVIEW = "overview";
-        final String MOVIE_RELEASE_DATE = "release_date";
-        StringBuilder moviesList = new StringBuilder();
-
-        @Override
-        protected Void doInBackground(ImageView... params) {
-// using the apikey in a separate string file to protect the apikey
-            try {
-                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + mContext.getResources().getString(R.string.apiKey));
-                // making url request and sending it to be read
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                // preparing a reader to go through the response
-                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                // below allows for controlled reading of potentially large text
-                String line;
-                while ((line = r.readLine()) != null) {
-                    total.append(line);
-                }
-                // checking if getting back something
-                Log.e("Response", total.toString());
-
-                //JSON section
-                popArray = new JSONObject(total.toString());
-                movies = popArray.getJSONArray(MOVIE_BLOCKS);
-                Log.e("movie titles", movies.toString());
-
-                // get movie poster filenames and put in an array
-                for (int i = 0; i < movies.length(); i++) {
-                    JSONObject aTitle = movies.getJSONObject(i);
-                    //could work
-//                    moviePathString[i] = aTitle.getString(MOVIE_POSTER);
-                    moviePosterAddress.add(aTitle.getString(MOVIE_POSTER));
-                    // check to see if working
-                    Log.e("poster path" + i, aTitle.getString(MOVIE_POSTER));
-                }
-                moviePathString = moviePosterAddress.toArray(new String[moviePosterAddress.size()]);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-
-        }
-    }
 }
--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,29 +1,96 @@
 package com.example.android.popularmovies;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
     public String[] text = {"first", "second"};
+    private ArrayList<String> moviePosterAddress = new ArrayList<>();
+    private JSONObject popArray;
+    private JSONArray movies;
+    final String MOVIE_POSTER = "poster_path";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new RequestPopularMovies().execute(null, null, null);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
+    }
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+    // added ImageView take it out if it doesn't work! 3/4
+    private class RequestPopularMovies extends AsyncTask<Void, Void, Void> {
+        StringBuilder total = new StringBuilder();
+        final String MOVIE_BLOCKS = "results";
+        final String MOVIE_TITLE = "original_title";
+        //        final String MOVIE_POSTER = "poster_path";
+        final String MOVIE_OVERVIEW = "overview";
+        final String MOVIE_RELEASE_DATE = "release_date";
+        StringBuilder moviesList = new StringBuilder();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // using the apikey in a separate string file to protect the apikey
+            try {
+                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + getResources().getString(R.string.apiKey));
+                // making url request and sending it to be read
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // preparing a reader to go through the response
+                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                // below allows for controlled reading of potentially large text
+                String line;
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                }
+                // checking if getting back something
+                Log.e("Response", total.toString());
+
+                //JSON section
+                popArray = new JSONObject(total.toString());
+                movies = popArray.getJSONArray(MOVIE_BLOCKS);
+                Log.e("movie titles", movies.toString());
+
+                // get movie poster filenames and put in an array
+                for (int i = 0; i < movies.length(); i++) {
+                    JSONObject aTitle = movies.getJSONObject(i);
+                    moviePosterAddress.add(aTitle.getString(MOVIE_POSTER));
+                    // check to see if working
+                    Log.e("poster path" + i, aTitle.getString(MOVIE_POSTER));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            GridView gridview = (GridView) findViewById(R.id.gridview);
+            gridview.setAdapter(new ImageAdapter(MainActivity.this, moviePosterAddress));
+
+            gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View v,
+                                        int position, long id) {
+                    Toast.makeText(MainActivity.this, "" + position,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
