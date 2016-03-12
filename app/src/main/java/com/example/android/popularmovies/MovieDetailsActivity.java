0d8c9b5f8b152fbecaf747f1abffa7bd4bbaab71
imdragon <i.m.dragon@gmail.com>
2016-03-11 18:10:12 -0800

    UI updates to detail view Added RatingBar Changed size of Movie Title textview to 34sp

--- app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -21,11 +22,11 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         TextView mTitle = (TextView) findViewById(R.id.original_title_detail);
         mTitle.setText(details.getTitle());
-        mTitle.setShadowLayer(25, 0, 0, Color.BLACK );
+        mTitle.setShadowLayer(25, 0, 0, Color.BLACK);
 
         ImageView mBackdrop = (ImageView) findViewById(R.id.backdropImageView);
         Picasso.with(this).load("http://image.tmdb.org/t/p/w780/"+details.getBackdrop()).into(mBackdrop);
-        Log.e("backdrop url", "http://image.tmdb.org/t/p/w780/"+details.getBackdrop());
+        Log.e("backdrop url", "http://image.tmdb.org/t/p/w780/" + details.getBackdrop());
         ImageView mPoster = (ImageView) findViewById(R.id.mPoster);
         Picasso.with(this).load("http://image.tmdb.org/t/p/w780/" + details.getPoster())
                 .placeholder(R.drawable.comingsoon).into(mPoster);
@@ -35,6 +36,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
 //        mRelease.setText(details.getReleaseDate());
         mRelease.setText("Released: "+details.getReleaseDate().substring(0,4));
         TextView mRating = (TextView) findViewById(R.id.ratingDetail);
+
+        RatingBar mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mRatingBar.setRating(Float.valueOf(details.getRating()));
         mRating.setText("Rating: "+details.getRating()+"/10");
 
 
